dd894a586b286b518364729adfaa26c8f2c32864
koustub manchiraju <koustub.247@gmail.com>
2020-02-05 03:20:51 -0500

    logic/css

--- quiz.js
+++ quiz.js
@@ -1,6 +1,11 @@
-
+window.onload = function()
+{
+    let count = 0;
+let score = 0;
 let ques = document.getElementById("qtn");
-let optns = document.getElementById("optns");
+let optn = document.getElementById("optns");
+let res = document.getElementById("result");
+let userAnswer = "";
 let questions =
 [
     {id:1,
@@ -15,46 +20,60 @@ let questions =
     },  
     {id:3,
     question:'which pet do you have?',
-    answer:'Apple',
+    answer:'cat',
     options:['Apple','cat','jackfruit','pineapple' ]
     } 
 ];
-
-pageLoad();
-
-// console.log(`${ques.innerText}`);
-// console.log(`${pqtn.innerHTML}`);
-
-function pageLoad(){
+printQuestion(count);
+optn.addEventListener('click',function(event){
     
-    ques.innerHTML = `<h2> Q: ${1}.${questions[0].question}<h2>`;
-    optns.innerHTML = `<ul>
-    <li id="0",">${questions[0].options[0]}</li> 
-    <li id="1">${questions[0].options[1]}</li> 
-    <li id="2">${questions[0].options[2]}</li> 
-    <li id="3">${questions[0].options[3]}</li> </ul>`;
-
-}
-let count = 1;
-optns.addEventListener('click',function(event){
-    
-    console.log("button is pressed");
-    var tid = document.getElementById('').innerHTML;
-    console.log(`${tid}`);
-    let optnsPrd = event.target.id;
-    console.log(`${optns.prsd}`); 
+    let btn_id = event.target.id;
+    //console.log(`${btn_id}`); 
+    userAnswer = document.getElementById(`${btn_id}`).innerHTML;
+    //console.log(`${userAnswer}`);
+    if(count == questions.length - 1){
+        console.log();
+    }
+    checkAnswer();
+    endQuiz();
     printQuestion(count);
-    count++;
-
+    
+    
 });
 function printQuestion(count){
-
-    ques.innerHTML = `<h2> Q:${count + 1}.${questions[count].question}<h2>`;
-    optns.innerHTML = `<ul>
-    <li>${questions[count].options[1]}</li> 
-    <li>${questions[count].options[2]}</li> 
-    <li>${questions[count].options[3]}</li> 
-    <li>${questions[count].options[4]}</li> </ul>`;
-
-
+    
+    ques.innerHTML =`<h2> Q:${count + 1}.${questions[count].question}<h2>`;
+    optns.innerHTML =`<ul>
+    <li id="1">${questions[count].options[0]}</li> 
+    <li id="2">${questions[count].options[1]}</li> 
+    <li id="3">${questions[count].options[2]}</li> 
+    <li id="4">${questions[count].options[3]}</li> </ul>`;
+    count++;
+    
+}
+function checkAnswer(){
+    
+    console.log(`${userAnswer}`);
+    var string1 = userAnswer;
+    console.log(`object answer is ${questions[count].answer}`);
+    var string2 = questions[count].answer;
+    var result = string1.localeCompare(string2);
+    console.log(`${result}`);
+    if(result == 0)
+    {
+        score +=1;
+        console.log(`you have the right`);
+        //res.innerHTML = 'right answer';
+        //setInterval(function(){
+          //  res.innerHTML = '';
+       // },1000)
+    }
+    else{
+        console.log(`you have the wrong`);
+        //res.innerHTML = 'wrong answer';
+    }
+}
+function endQuiz(){
+        
+}
 }
