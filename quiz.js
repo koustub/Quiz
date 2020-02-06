500f1c7267fe5b7cb18688e386f21c250911c5e4
koustub manchiraju <koustub.247@gmail.com>
2020-02-05 22:59:12 -0500

    code for Timer/endPage/AnswerCheck

--- quiz.js
+++ quiz.js
@@ -1,11 +1,3 @@
-window.onload = function()
-{
-    let count = 0;
-let score = 0;
-let ques = document.getElementById("qtn");
-let optn = document.getElementById("optns");
-let res = document.getElementById("result");
-let userAnswer = "";
 let questions =
 [
     {id:1,
@@ -24,32 +16,23 @@ let questions =
     options:['Apple','cat','jackfruit','pineapple' ]
     } 
 ];
+let count = 0;
+let score = 0;
+let ques = document.getElementById("qtn");
+let optn = document.getElementById("optns");
+let interval;
+let countTimer = 30;
+let userAnswer = "";
+
 printQuestion(count);
-optn.addEventListener('click',function(event){
-    
-    let btn_id = event.target.id;
-    //console.log(`${btn_id}`); 
-    userAnswer = document.getElementById(`${btn_id}`).innerHTML;
-    //console.log(`${userAnswer}`);
-    if(count == questions.length - 1){
-        console.log();
-    }
-    checkAnswer();
-    endQuiz();
-    printQuestion(count);
-    
-    
-});
+startTimer();
 function printQuestion(count){
-    
     ques.innerHTML =`<h2> Q:${count + 1}.${questions[count].question}<h2>`;
     optns.innerHTML =`<ul>
     <li id="1">${questions[count].options[0]}</li> 
     <li id="2">${questions[count].options[1]}</li> 
     <li id="3">${questions[count].options[2]}</li> 
     <li id="4">${questions[count].options[3]}</li> </ul>`;
-    count++;
-    
 }
 function checkAnswer(){
     
@@ -63,17 +46,55 @@ function checkAnswer(){
     {
         score +=1;
         console.log(`you have the right`);
-        //res.innerHTML = 'right answer';
-        //setInterval(function(){
-          //  res.innerHTML = '';
-       // },1000)
-    }
-    else{
-        console.log(`you have the wrong`);
-        //res.innerHTML = 'wrong answer';
-    }
-}
-function endQuiz(){
+        var res = document.getElementById('result');
+        res.innerHTML = "<h2>Right Answer</h2>";
+        setTimeout(function(){
+            res.innerHTML = "<h2></h2>";
+        }, 200);
         
+    }
+    else
+    {
+        console.log(`you have the wrong`);
+        countTimer -= 5;
+        var res = document.getElementById('result');
+        res.innerHTML = "<h2>wrong Answer  </h2>";
+        setTimeout(function(){
+            res.innerHTML = "";
+        }, 2000); 
+    }
+    console.log(`your score ${score}`);
 }
+
+function startTimer() {
+    qt = document.getElementById('quizTime');
+    interval = setInterval(function () {
+        qt.innerHTML = countTimer;
+        countTimer--;
+        if (countTimer <= 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
 }
+function checkRun(){
+    count++;
+    if (count == questions.length) 
+    {
+        console.log('end of your quiz');
+        // console.log('you have entered another room');
+        // let fRes = document.getElementById("userResult");
+        // fRes.innerHTML = `<h2> your Score is ${score}`;
+        location.href = "end.html";
+        return;
+    }
+    printQuestion(count);
+}
+optn.addEventListener('click',function(event)
+{
+    let btn_id = event.target.id;
+    userAnswer = document.getElementById(`${btn_id}`).innerHTML;
+    checkAnswer();
+    checkRun();
+    
+});
+
